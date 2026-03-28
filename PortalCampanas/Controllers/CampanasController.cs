66b665a492493e07fa2f0aac5d7c541ec61d123f
SandraPanez <hobsalec@gmail.com>
2026-03-28 12:37:13 -0500

    implementar listado de campañas

---
+++ PortalCampanas/Controllers/CampanasController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using PortalCampanas.Services;
+
+namespace PortalCampanas.Controllers
+{
+    public class CampanasController : Controller
+    {
+        private readonly CampanaService _service;
+
+        public CampanasController(CampanaService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var campanas = _service.ObtenerTodas();
+            return View(campanas);
+        }
+    }
+}
